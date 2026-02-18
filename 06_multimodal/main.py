9ae9ee030034f0383ce23b07e1729efeb16761d0
DreamerAkhilesh <imakhileshpratap@gmail.com>
2026-02-19 00:03:18 +0530

    adding image

--- 06_multimodal/main.py
+++ 06_multimodal/main.py
@@ -13,7 +13,7 @@ client.chat.completions.create(
       "role": "user",
       "content": [
         { "type": "text", "text": "Generate a caption for this image in about 50 words" },
-        { "type": "image_url", "image_url": { "url": "" } }
+        { "type": "image_url", "image_url": { "url": "https://images.pexels.com/photos/12899196/pexels-photo-12899196.jpeg" } }
       ]
     }
   ]
