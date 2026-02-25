cdaf5a4385a1530fde91df420e3ea6b2c383c4b4
DreamerAkhilesh <imakhileshpratap@gmail.com>
2026-02-25 22:20:11 +0530

    updating th echat.py with the nodes, states

--- 07_lang-graph/chat.py
+++ 07_lang-graph/chat.py
@@ -4,6 +4,17 @@
 from typing_extensions import TypedDict
 from typing import Annotated
 from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph
 
 class State(TypedDict):
     messages : Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State) 
+
+def chatbot (state : State) :
+    return {"message" : ["Hi! This is a message from chatbot"]} 
+    # will take the previous state
+    # will get appended the messages to the previous state
+
+graph_builder.add_node("Chat-bot", chatbot)
